5a6680f165d06cf4e928743af026621b8721d243
Oliver Batchelor <saulzar@gmail.com>
2020-04-23 17:03:37 +1200

    More comprehensive setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 setup(
-    name="node_expressions",
-    version="0.1",
-    packages=['node'],
-    install_requires = [
-        "cached-property",
-        "future"
-    ]
+
+    name="node-expressions",
+    version="0.0.1",
+    author="Oliver Batchelor",
+    author_email="saulzar@gmail.com",
+    description="API for using expressions to create node graphs in blender",
+    url="https://github.com/saulzar/node_expressions",
+    packages=find_namespace_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
