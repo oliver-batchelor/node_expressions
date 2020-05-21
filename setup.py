16eb2ab54f18ecadfdd24df96734d89fb363478b
Oliver Batchelor <saulzar@gmail.com>
2020-05-22 03:04:50 +1200

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 setup(
 
     name="node-expressions",
-    version="0.0.1",
+    version="0.0.3",
     author="Oliver Batchelor",
     author_email="saulzar@gmail.com",
     description="API for using expressions to create node graphs in blender",
@@ -12,6 +12,9 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        "fuzzywuzzy"
     ],
     python_requires='>=3.7',
 )
